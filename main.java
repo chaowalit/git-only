4625323240e3e458122b78fdfc07575c8c4f0e10
chaowalit.kon <dekchaowalit_123@hotmail.com>
2017-05-06 13:42:07 +0700

    last

--- main.java
+++ main.java
@@ -3,6 +3,14 @@ class Main {
 		System.out.println("hhh");
 		System.out.println("hhh11");
 		System.out.println("hhh22");
+		for (int i = 1 ; i < 10 ; i++){
+			System.out.println("test1");
+			String myname = "nong" + i;
+			Person person = new Person();
+			System.out.println("test2");
+			System.out.println("test3");
+			System.out.println("test4");
+		}
 		System.out.println("bank2");
 		String str = "";
 		System.out.println("hxhh333");
